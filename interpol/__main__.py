1cf331e85a8af6277d831db263eff8a0cee5deed
radium226 <adrien.besnard@gmail.com>
2015-01-24 17:32:47 +0100

    Adding __main__

---
+++ interpol/__main__.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+
+from interpolate import Interpolate
+import sys
+
+def iterate_stdin():
+    while True: 
+        line = sys.stdin.readline()
+        if line == "":
+            break
+        yield line
+        
+if __name__ == "__main__":
+    interpolate = Interpolate(1, 100, 1)
+    for p in interpolate(enumerate(map(lambda l: None if l == "" else float(l), map(lambda line: line.rstrip(), iterate_stdin())))):
+        print("%i" % p[1])
+        
